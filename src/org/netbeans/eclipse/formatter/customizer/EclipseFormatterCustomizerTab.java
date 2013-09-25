cf629270e954ecb9da125a1a4eedef2b45884ed9
markiewb <benno.markiewicz@googlemail.com>
2013-09-25 23:00:10 +0200

    [license] add header to source files

--- src/org/netbeans/eclipse/formatter/customizer/EclipseFormatterCustomizerTab.java
+++ src/org/netbeans/eclipse/formatter/customizer/EclipseFormatterCustomizerTab.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2013 markiewb.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    markiewb - initial API and implementation and/or initial documentation
+ */
 package org.netbeans.eclipse.formatter.customizer;
 
 import java.awt.event.ActionEvent;
--- src/org/netbeans/eclipse/formatter/customizer/VerifiableConfigPanel.java
+++ src/org/netbeans/eclipse/formatter/customizer/VerifiableConfigPanel.java
@@ -1,9 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2013 markiewb.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    markiewb - initial API and implementation and/or initial documentation
  */
-
 package org.netbeans.eclipse.formatter.customizer;
 
 /**
