96f215bb4919403eaab4d7bfb178dc4942ea48a5
markiewb <benno.markiewicz@googlemail.com>
2015-07-20 09:52:51 +0200

    Use jars from new eclipse 4.5 version #63 - duplicate org.eclipse.equinox.log.LogFilter and add org.eclipse.core.runtime

--- test/unit/src/de/markiewb/netbeans/plugins/eclipse/formatter/NewEmptyJUnitTest.java
+++ test/unit/src/de/markiewb/netbeans/plugins/eclipse/formatter/NewEmptyJUnitTest.java
@@ -1,5 +1,6 @@
 package de.markiewb.netbeans.plugins.eclipse.formatter;
 
+import static junit.framework.Assert.assertFalse;
 import org.junit.Test;
 
 /**
@@ -11,8 +12,9 @@ public class NewEmptyJUnitTest {
     @Test
     public void testFormat() {
         EclipseFormatter formatter = new EclipseFormatter("D:\\ws\\eclipsecodeformatter_for_netbeans\\test\\unit\\src\\formattersampleeclipse.xml", "eclipse-demo");
-        final String text = "package foo;\n\npublic class NewEmptyJUnitTest {}";
-        formatter.forCode(text, 0, text.length() - 1);
+        final String text = "package foo;public class NewEmptyJUnitTest {}";
+        String actual = formatter.forCode(text, 0, text.length() - 1);
+        assertFalse("Formatting should change the code", actual.equals(text));
     }
 
 }
