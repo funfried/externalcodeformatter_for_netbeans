4391cf76d243498544ceffa500ecf38b23c95dae
fbahle <fabian.bahle@mapal.com>
2019-12-03 23:04:01 +0100

    - Removed default format action and added default shortcut for custom format action - Renamed format action from "Format with external formatter" to "Format"

--- src/main/java/de/funfried/netbeans/plugins/external/formatter/ui/actions/FormatAction.java
+++ src/main/java/de/funfried/netbeans/plugins/external/formatter/ui/actions/FormatAction.java
@@ -10,9 +10,6 @@
  */
 package de.funfried.netbeans.plugins.external.formatter.ui.actions;
 
-import de.funfried.netbeans.plugins.external.formatter.strategies.FormatterStrategyDispatcher;
-import de.funfried.netbeans.plugins.external.formatter.strategies.FormatterAdvice;
-
 import java.awt.event.ActionEvent;
 
 import javax.swing.text.JTextComponent;
@@ -22,6 +19,9 @@ import org.netbeans.api.editor.EditorActionRegistration;
 import org.netbeans.editor.BaseAction;
 import org.openide.util.NbBundle;
 
+import de.funfried.netbeans.plugins.external.formatter.strategies.FormatterAdvice;
+import de.funfried.netbeans.plugins.external.formatter.strategies.FormatterStrategyDispatcher;
+
 /**
  * Registration analog to
  * http://hg.netbeans.org/jet-main/file/01c13d4da2da/java.hints/src/org/netbeans/modules/java/hints/OrganizeMembers.java
@@ -29,7 +29,7 @@ import org.openide.util.NbBundle;
  * @author markiewb
  * @author bahlef
  */
-@NbBundle.Messages({ "external-format=Format with external formatter" })
+@NbBundle.Messages({ "external-format=Format" })
 @EditorActionRegistration(category = "Source", name = FormatAction.MACRONAME, menuPath = "Source", menuPosition = 299, menuText = "#external-format", popupPath = "", popupPosition = 1599, popupText = "#external-format")
 public class FormatAction extends BaseAction {
 	private static final long serialVersionUID = 1L;
