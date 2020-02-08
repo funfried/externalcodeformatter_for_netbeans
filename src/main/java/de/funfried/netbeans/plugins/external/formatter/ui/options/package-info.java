8f8ba4a526c97d8eca7dbb255b5806d9fb1ce525
fbahle <fabian.bahle@mapal.com>
2020-02-08 23:12:48 +0100

    JavaDoc improved, switched from package.html to package-info.java for package docs

---
+++ src/main/java/de/funfried/netbeans/plugins/external/formatter/ui/options/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 bahlef.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * Contributors:
+ * bahlef - initial API and implementation and/or initial documentation
+ */
+
+/**
+ * Package containing classes for the External Java Formatter option panel.
+ */
+package de.funfried.netbeans.plugins.external.formatter.ui.options;
