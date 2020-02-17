76ea1bb1a77d43ecb0f9ce2d4ae9e967ac59189b
fbahle <info@funfried.de>
2020-02-17 21:41:04 +0100

    #101 Added unit tests for new Spring Java formatter feature

---
+++ src/test/java/de/funfried/netbeans/plugins/external/formatter/java/spring/SpringJavaFormatterServiceTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2020 bahlef.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * Contributors:
+ * bahlef - initial API and implementation and/or initial documentation
+ */
+package de.funfried.netbeans.plugins.external.formatter.java.spring;
+
+import javax.swing.text.StyledDocument;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.netbeans.junit.NbTestCase;
+import org.netbeans.modules.editor.NbEditorDocument;
+
+/**
+ *
+ * @author bahlef
+ */
+public class SpringJavaFormatterServiceTest extends NbTestCase {
+	public SpringJavaFormatterServiceTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Test of format method, of class {@link SpringJavaFormatterService}.
+	 *
+	 * @throws Exception if an error occurs
+	 */
+	@Test
+	public void testFormat() throws Exception {
+		final String text = "package foo;public enum NewEmptyJUnitTest {A,B,C}\n";
+		final String expected = "package foo;\n" +
+				"\n" +
+				"public enum NewEmptyJUnitTest {\n" +
+				"\n" +
+				"	A, B, C\n" +
+				"\n" +
+				"}\n" +
+				"";
+
+		StyledDocument document = new NbEditorDocument("text/x-java");
+		document.insertString(0, text, null);
+
+		SpringJavaFormatterService instance = new SpringJavaFormatterService();
+		instance.format(document, null);
+
+		String actual = document.getText(0, document.getLength());
+
+		Assert.assertEquals("Formatting should change the code", expected, actual);
+	}
+
+	/**
+	 * Test of {@link EclipseJavaFormatterService#format(javax.swing.text.StyledDocument, java.util.SortedSet)}
+	 * method, of class {@link EclipseJavaFormatterService}.
+	 *
+	 * @throws Exception if an error occurs
+	 */
+	@Test
+	public void testUnsupportedFileType() throws Exception {
+		final String text = "package foo;public enum NewEmptyJUnitTest {A,B,C}\n";
+
+		StyledDocument document = new NbEditorDocument("text/xml");
+		document.insertString(0, text, null);
+
+		SpringJavaFormatterService instance = new SpringJavaFormatterService();
+
+		try {
+			instance.format(document, null);
+		} catch (Exception ex) {
+			Assert.assertTrue("Formatting should not be possible for the given file type", ex.getMessage().contains("The file type 'text/xml' is not supported"));
+		}
+	}
+}
---
+++ src/test/java/de/funfried/netbeans/plugins/external/formatter/java/spring/SpringJavaFormatterWrapperTest.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2020 bahlef.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * Contributors:
+ * markiewb - initial API and implementation and/or initial documentation
+ * bahlef
+ */
+package de.funfried.netbeans.plugins.external.formatter.java.spring;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.funfried.netbeans.plugins.external.formatter.ui.options.Settings;
+
+/**
+ *
+ * @author bahlef
+ */
+public class SpringJavaFormatterWrapperTest {
+	private static SpringJavaFormatterWrapper instance;
+
+	@BeforeClass
+	public static void setUpClass() {
+		instance = new SpringJavaFormatterWrapper();
+	}
+
+	@Test
+	public void testFormatUsingXML() {
+		final String text = "package foo;public enum NewEmptyJUnitTest {A,B,C}";
+		final String expected = "package foo;\n"
+				+ "\n"
+				+ "public enum NewEmptyJUnitTest {\n"
+				+ "\n"
+				+ "	A, B, C\n"
+				+ "\n"
+				+ "}";
+
+		String actual = instance.format(text, null, null);
+		Assert.assertEquals("Formatting should change the code", expected, actual.replaceAll("\r", ""));
+	}
+
+	@Test
+	public void testFormatUsingXML_formatterOnOff() {
+		final String text = "package foo; public class NewEmptyJUnitTest {\n" +
+				"\n" +
+				"// @formatter:off\n" +
+				"public void doSomething(String arg) {\n" +
+				"	System.out.println(\"Hello World\");\n" +
+				"}\n" +
+				"// @formatter:on\n" +
+				"\n" +
+				"public boolean doSomethingElse(Object obj) { return false; } }";
+		final String expected = "package foo;\n" +
+				"\n" +
+				"public class NewEmptyJUnitTest {\n" +
+				"\n" +
+				"// @formatter:off\n" +
+				"public void doSomething(String arg) {\n" +
+				"	System.out.println(\"Hello World\");\n" +
+				"}\n" +
+				"// @formatter:on\n" +
+				"\n" +
+				"	public boolean doSomethingElse(Object obj) {\n" +
+				"		return false;\n" +
+				"	}\n" +
+				"\n" +
+				"}";
+
+		String actual = instance.format(text, null, null);
+		Assert.assertEquals("Formatting should change the code", expected, actual.replaceAll("\r", ""));
+	}
+
+	@Test
+	public void testFormatUsingLinefeed_CR() {
+		final String text = "package foo;public enum NewEmptyJUnitTest { A, B, C}";
+		final String expected = "package foo;\r"
+				+ "\r"
+				+ "public enum NewEmptyJUnitTest {\r"
+				+ "\r"
+				+ "	A, B, C\r"
+				+ "\r"
+				+ "}";
+
+		String actual = instance.format(text, Settings.getLineFeed("\\r", null), null);
+		Assert.assertEquals("Formatting should change the code", expected, actual);
+	}
+
+	@Test
+	public void testFormatUsingLinefeed_LF() {
+		final String text = "package foo;public enum NewEmptyJUnitTest { A, B, C}";
+		final String expected = "package foo;\n"
+				+ "\n"
+				+ "public enum NewEmptyJUnitTest {\n"
+				+ "\n"
+				+ "	A, B, C\n"
+				+ "\n"
+				+ "}";
+
+		String actual = instance.format(text, Settings.getLineFeed("\\n", null), null);
+		Assert.assertEquals("Formatting should change the code", expected, actual);
+	}
+
+	@Test
+	public void testFormatUsingLinefeed_CRLF() {
+		final String text = "package foo;public enum NewEmptyJUnitTest { A, B, C}";
+		final String expected = "package foo;\r\n"
+				+ "\r\n"
+				+ "public enum NewEmptyJUnitTest {\r\n"
+				+ "\r\n"
+				+ "	A, B, C\r\n"
+				+ "\r\n"
+				+ "}";
+
+		String actual = instance.format(text, Settings.getLineFeed("\\r\\n", null), null);
+		Assert.assertEquals("Formatting should change the code", expected, actual);
+	}
+
+	@Test
+	public void testNullCode() {
+		String actual = instance.format(null, null, null);
+		Assert.assertNull("Formatting shouldn't change the code, should still be null", actual);
+	}
+
+	@Test
+	public void testIllegalChangedElementsCode() {
+		final String text = "package foo;public enum NewEmptyJUnitTest {A,B,C}";
+
+		SortedSet<Pair<Integer, Integer>> regions = new TreeSet<>();
+		regions.add(Pair.of(1200, 1230));
+
+		try {
+			instance.format(text, null, regions);
+
+			Assert.assertFalse("Should have thrown an IllegalArgumentException", true);
+		} catch (IllegalArgumentException ex) {
+			Assert.assertTrue(true);
+		}
+	}
+}
