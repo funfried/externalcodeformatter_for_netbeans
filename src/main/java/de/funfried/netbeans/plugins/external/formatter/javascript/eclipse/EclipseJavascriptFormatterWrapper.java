9f5d2d2da6919919f23e24806618a9f76449693a
fbahle <info@funfried.de>
2020-06-21 15:30:12 +0200

    Fixed small bug in formatting Java code

--- src/main/java/de/funfried/netbeans/plugins/external/formatter/javascript/eclipse/EclipseJavascriptFormatterWrapper.java
+++ src/main/java/de/funfried/netbeans/plugins/external/formatter/javascript/eclipse/EclipseJavascriptFormatterWrapper.java
@@ -97,8 +97,6 @@ public final class EclipseJavascriptFormatterWrapper {
 				if (Objects.equals(code, formattedCode)) {
 					return null;
 				}
-			} else {
-				throw new FormattingFailedException("Formatting the given code ended in a null result.");
 			}
 		} catch (FormattingFailedException | IllegalArgumentException ex) {
 			throw ex;
